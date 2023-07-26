41ab1fc2ef470c0010e372f8ee13be173149c8c8
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 07:04:57 +0800

    Add UserOverview section to Workspace

--- src/routes/Workspace.jsx
+++ src/routes/Workspace.jsx
@@ -1,11 +1,14 @@
 import './Page.scss';
 import useAuth from '@/hooks/useAuth';
+import useWorkspace from '@/hooks/useWorkspace';
 import RedirectToLogin from '@/components/redirects/RedirectToLogin';
+import UserOverview from '@/components/overviews/UserOverview';
 
 function Workspace() {
-  const { user } = useAuth();
+  const { user: currentUser } = useAuth();
+  const { users } = useWorkspace();
 
-  if (!user) return <RedirectToLogin />;
+  if (!currentUser) return <RedirectToLogin />;
 
   return (
     <main className="main" aria-labelledby="workspace-title">
@@ -13,9 +16,17 @@ function Workspace() {
         <h1 className="main-title" id="workspace-title">
           Your Workspace
         </h1>
+        <p className="main-subtitle">
+          See what everyone is up to at this time!
+        </p>
 
         <section className="main-section">
           {/* Panel for members of workspace */}
+          {users.length > 0 ? (
+            users.map((user) => <UserOverview key={user.id} user={user} />)
+          ) : (
+            <p className="main-empty">There are no users here</p>
+          )}
         </section>
       </div>
     </main>
