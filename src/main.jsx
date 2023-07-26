a3f182bd20b53d325ab2ecc8a55f7b3361cbf4e8
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 00:42:27 +0800

    Create app router and add account routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import Signup from './routes/account/Signup';
+import Login from './routes/account/Login';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="account">
+        <Route index element={<div>Account</div>} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Route>,
+  ),
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className="app">HELLO</div>
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 );
