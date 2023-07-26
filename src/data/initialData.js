19c3795cade198ad1580aec4e82534b5e030af8b
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 03:33:36 +0800

    Create initial project list

---
+++ src/data/initialData.js
@@ -0,0 +1,37 @@
+export const initProjects = [
+  {
+    id: 'proj__h25g4ya',
+    title: 'Healthier Options',
+    slug: 'healthier-options',
+    client: 'Artisan Group',
+    color: 'rgba(176, 219, 67, 0.5)',
+  },
+  {
+    id: 'proj__t7633xd',
+    title: 'EcoConnect',
+    slug: 'ecoconnect',
+    client: 'Barristan Asia',
+    color: 'rgba(255, 46, 0, 0.5)',
+  },
+  {
+    id: 'proj__s1t467',
+    title: 'Fitness Buddy',
+    slug: 'fitness-buddy',
+    client: 'Cill Group of Companies',
+    color: 'rgba(254, 168, 47, 0.5)',
+  },
+  {
+    id: 'proj__23fdh5',
+    title: 'Travelista',
+    slug: 'travelista',
+    client: 'Fusion & Grow',
+    color: 'rgba(170, 69, 134, 0.5)',
+  },
+  {
+    id: 'proj__55fsvth',
+    title: 'Kashew Tanar',
+    slug: 'kashew-tanar',
+    client: 'Comprehensive Solutions',
+    color: 'rgba(125, 206, 130, 0.5)',
+  },
+];
