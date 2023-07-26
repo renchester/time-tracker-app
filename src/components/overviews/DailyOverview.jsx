74ab95df6d472d4d588540dbef37fab5b2272952
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 05:34:52 +0800

    Add sameDay checkers for DailyOverview

--- src/components/overviews/DailyOverview.jsx
+++ src/components/overviews/DailyOverview.jsx
@@ -1,6 +1,6 @@
 import './DailyOverview.scss';
 import PropTypes from 'prop-types';
-import { format } from 'date-fns';
+import { format, isSameDay } from 'date-fns';
 import { taskType } from '@/types/types';
 import { getTotalHours } from '@/lib/dateHelpers';
 import TaskDailyCard from '../cards/TaskDailyCard';
@@ -8,7 +8,8 @@ import TaskDailyCard from '../cards/TaskDailyCard';
 function DailyOverview(props) {
   const { day, tasks } = props;
   const elementID = `daily-ov__${day.toISOString()}`;
-  const dateFormat = 'eee, LLL dd';
+  const sameDayStatus = isSameDay(new Date(), new Date(day));
+  const dateFormat = sameDayStatus ? 'LLL dd' : 'eee, LLL dd';
 
   // Sort tasks by startTime
   const sortedTasks = tasks.sort((a, b) => {
@@ -19,10 +20,18 @@ function DailyOverview(props) {
   const totalHours = getTotalHours(sortedTasks);
 
   return (
-    <section className="daily-ov" aria-labelledby={elementID}>
+    <section
+      className="daily-ov"
+      aria-labelledby={elementID}
+      data-now={sameDayStatus}
+    >
       <div className="daily-ov__header">
-        <h5 id={elementID} className="daily-ov__day">
-          {format(new Date(day), dateFormat)}
+        <h5
+          id={elementID}
+          className="daily-ov__day"
+          data-active={sameDayStatus}
+        >
+          {sameDayStatus && 'Today,'} {format(new Date(day), dateFormat)}
         </h5>
         <span className="daily-ov__total">{totalHours}h</span>
       </div>
