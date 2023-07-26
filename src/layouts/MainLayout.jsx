6a861ee0f3afe67a458f5f6cfdd005164c49487c
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 03:49:53 +0800

    Add NewTaskProvider to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,23 @@
 import { Outlet } from 'react-router-dom';
 import { AuthProvider } from '@/context/AuthContext';
 import { SnackbarProvider } from '@/context/SnackbarContext';
-import Sidebar from '@/components/Sidebar';
 import { WorkspaceProvider } from '@/context/WorkspaceContext';
+import { NewTaskProvider } from '@/context/NewTaskContext';
 import AddTaskButton from '@/components/factories/AddTaskButton';
+import Sidebar from '@/components/Sidebar';
 
 function MainLayout() {
   return (
     <AuthProvider>
       <WorkspaceProvider>
         <SnackbarProvider>
-          <div className="main-layout">
-            <Sidebar />
-            <Outlet />
-            <AddTaskButton />
-          </div>
+          <NewTaskProvider>
+            <div className="main-layout">
+              <Sidebar />
+              <Outlet />
+              <AddTaskButton />
+            </div>
+          </NewTaskProvider>
         </SnackbarProvider>
       </WorkspaceProvider>
     </AuthProvider>
