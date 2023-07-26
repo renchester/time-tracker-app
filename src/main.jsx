427c3303dcbdcaf6e03b115fbcdb1eba2a7b2267
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-27 02:30:56 +0800

    Add main routes and respective components

--- src/main.jsx
+++ src/main.jsx
@@ -11,10 +11,17 @@ import {
 import MainLayout from './layouts/MainLayout';
 import Signup from './routes/account/Signup';
 import Login from './routes/account/Login';
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+import Workspace from './routes/Workspace';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="projects" element={<Projects />} />
+      <Route path="workspace" element={<Workspace />} />
+
       <Route path="account">
         <Route index element={<div>Account</div>} />
         <Route path="signup" element={<Signup />} />
